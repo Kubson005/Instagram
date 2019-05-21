48fa280c3478df3cb42f219c1f4556f81e548203
Dawid <dawid.debo2.pl@gmail.com>
2019-05-21 19:51:16 +0200

    Foto i tekst

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const User = props => (
+  <div className="user">
+    <img className="user-photo" src={props.link}alt={props.alt}/>
+    <p className="userName">{props.userName}</p>
     </div>
-  );
-}
+)
 
-export default App;
+const Gallery = props => (
+  <div>
+    {props.gallery.map( 
+      el => <img key={el.id} 
+      src={el.link}
+      alt={el.alt}
+      /> )}
+  </div>
+)
+
+export default class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      userName : "Kubson",
+      profilPhoto : "https://bit.ly/2waS9Gj",
+      gallery : [
+        {
+          id: 0,
+          link: "https://bit.ly/2O3PQvr",
+          alt: "Pies",
+        },
+        {
+          id: 1,
+          link: "https://bit.ly/2YCP1iP",
+          alt: "Piesek",
+        },
+        {
+          id: 2,
+          link: "https://bit.ly/30zqcWU",
+          alt: "Pieseł",
+        },
+      ]
+    }
+  }
+  render(){
+    return(
+      <main>
+        <User link={this.state.profilPhoto}
+              alt= "Profilowe"
+              userName={this.state.userName}/>
+        <Gallery gallery={this.state.gallery}/>
+      </main>
+    )
+    }
+  }
+
+
+
